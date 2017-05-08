fba7e1a3f54cf0969b20717521963fa0450f5887
Vincent Lee <cool8511@gmail.com>
2017-05-08 10:44:40 +0800

    bump version

--- common/binary.go
+++ common/binary.go
@@ -5,7 +5,7 @@ import (
 	"runtime"
 )
 
-const VerBinary = "0.0.1"
+const VerBinary = "0.0.2"
 
 func VerString(app string) string {
 	return fmt.Sprintf("%s v%s (built w/%s)", app, VerBinary, runtime.Version())
