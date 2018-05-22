35e978ef3145ee6dcd0248298b83b7075e7d52c7
Vincent Lee <cool8511@gmail.com>
2018-05-22 23:58:50 +0800

    use dep

--- dist.sh
+++ dist.sh
@@ -11,7 +11,7 @@
 set -e
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-export GOPATH=$DIR/.godeps:$(go env GOPATH)
+#export GOPATH=$DIR/.godeps:$(go env GOPATH)
 echo $GOPATH
 
 arch=$(go env GOARCH)
--- pre-dist.sh
+++ pre-dist.sh
@@ -4,12 +4,8 @@ set -e
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 rm -rf   $DIR/dist/docker
-rm -rf   $DIR/.godeps/src/github.com
-mkdir -p $DIR/.godeps
+rm -rf   $DIR/.godeps
 mkdir -p $DIR/dist
-export GOPATH=$DIR/.godeps:$(go env GOPATH)
-GOPATH=$DIR/.godeps gpm get
 
 arch=$(go env GOARCH)
-
-#go test -tags=embed -race ./...
+dep ensure
