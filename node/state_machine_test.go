ff74d66740111ac3be7dbf15c95c1595b80d90c9
Vincent Lee <cool8511@gmail.com>
2019-06-26 06:02:31 +0800

    fix panic while get stats if node is stopped or recovering

---
+++ node/state_machine_test.go
@@ -0,0 +1,99 @@
+package node
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_handleReuseOldCheckpoint(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("", fmt.Sprintf("sm-test-%d", time.Now().UnixNano()))
+	assert.Nil(t, err)
+	//defer os.RemoveAll(tmpDir)
+	t.Logf("dir:%v\n", tmpDir)
+	terms := []uint64{
+		0x0001,
+		0x0002,
+		0x0003,
+		0x011a,
+		0x021a,
+		0x031a,
+	}
+	indexs := []uint64{
+		0x01000,
+		0x02000,
+		0x03000,
+		0x0a000,
+		0x0b000,
+		0x0c000,
+	}
+	cntIdx := 10
+	type args struct {
+		srcInfo    string
+		localPath  string
+		term       uint64
+		index      uint64
+		skipReuseN int
+	}
+	destPath1 := path.Join(tmpDir, fmt.Sprintf("%016x-%016x", 1, 1))
+	destPath2 := path.Join(tmpDir, fmt.Sprintf("%016x-%016x", 2, 2))
+	tests := []struct {
+		name  string
+		args  args
+		want  string
+		want1 string
+	}{
+		{"src match in first", args{"matchfirst", tmpDir, 1, 1, 0}, path.Join(tmpDir, "031a-0c009"), destPath1},
+		{"src match in last", args{"matchlast", tmpDir, 1, 1, 0}, path.Join(tmpDir, "0001-01000"), destPath1},
+		{"src not match", args{"notmatch", tmpDir, 1, 1, 0}, "", destPath1},
+		{"src match in middle", args{"matchmiddle", tmpDir, 1, 1, 0}, path.Join(tmpDir, "031a-0c005"), destPath1},
+		{"src match in same with old", args{"matchfirst", tmpDir, 0x01ba, 0x02ab46c3, 0}, path.Join(tmpDir, "031a-0c009"), path.Join(tmpDir, "00000000000001ba-0000000002ab46c3")},
+		{"src match first skip 1", args{"matchfirst", tmpDir, 2, 2, 1}, "", destPath2},
+		{"src match last skip 1", args{"matchlast", tmpDir, 2, 2, 1}, "", destPath2},
+		{"src match middle skip 1", args{"matchmiddle", tmpDir, 2, 2, 1}, path.Join(tmpDir, "021a-0b005"), destPath2},
+		{"src match middle skip 2", args{"matchmiddle", tmpDir, 2, 2, 2}, path.Join(tmpDir, "011a-0a005"), destPath2},
+		{"src match middle skip all", args{"matchmiddle", tmpDir, 2, 2, len(terms)}, "", destPath2},
+	}
+
+	// source info
+	for i := 0; i < len(terms); i++ {
+		for j := 0; j < cntIdx; j++ {
+			idx := indexs[i] + uint64(j)
+			p := path.Join(tmpDir, fmt.Sprintf("%04x-%05x", terms[i], idx))
+			err := os.MkdirAll(p, 0777)
+			assert.Nil(t, err)
+			if i == 0 && j == 0 {
+				postFileSync(p, "matchlast")
+				t.Logf("gen: %v, %v", p, "matchlast")
+			} else if j == cntIdx/2 {
+				postFileSync(p, "matchmiddle")
+				t.Logf("gen: %v, %v", p, "matchmiddle")
+			} else if i == len(terms)-1 && j == cntIdx-1 {
+				postFileSync(p, "matchfirst")
+				t.Logf("gen: %v, %v", p, "matchfirst")
+			} else {
+				t.Logf("gen: %v, no src info", p)
+			}
+		}
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := handleReuseOldCheckpoint(tt.args.srcInfo, tt.args.localPath, tt.args.term, tt.args.index, tt.args.skipReuseN)
+			if got != tt.want {
+				t.Errorf("handleReuseOldCheckpoint() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("handleReuseOldCheckpoint() got1 = %v, want %v", got1, tt.want1)
+			}
+			// rename back to normal to test next
+			if got != "" {
+				os.Rename(got1, got)
+			}
+		})
+	}
+}
