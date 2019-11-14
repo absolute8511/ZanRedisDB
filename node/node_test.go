a82aad03bf1b9beeaa1b01471a101edd671b667f
Vincent Lee <cool8511@gmail.com>
2019-11-14 12:18:22 +0800

    optimize the wait req pool

---
+++ node/node_test.go
@@ -0,0 +1,35 @@
+package node
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWaitReqPools(t *testing.T) {
+	wrPools := newWaitReqPoolArray()
+
+	wr := wrPools.getWaitReq(1)
+	assert.Equal(t, minPoolIDLen, cap(wr.ids))
+	wr = wrPools.getWaitReq(minPoolIDLen)
+	assert.Equal(t, minPoolIDLen, cap(wr.ids))
+	wr = wrPools.getWaitReq(minPoolIDLen + 1)
+	assert.Equal(t, minPoolIDLen*2, cap(wr.ids))
+	wr = wrPools.getWaitReq(minPoolIDLen * 2)
+	assert.Equal(t, minPoolIDLen*2, cap(wr.ids))
+	wr = wrPools.getWaitReq(minPoolIDLen*2 + 1)
+	assert.Equal(t, minPoolIDLen*2*2, cap(wr.ids))
+	wr = wrPools.getWaitReq(minPoolIDLen * 2 * 2)
+	assert.Equal(t, minPoolIDLen*2*2, cap(wr.ids))
+	wr = wrPools.getWaitReq(minPoolIDLen*2*2 + 1)
+	assert.Equal(t, minPoolIDLen*2*2*2, cap(wr.ids))
+	wr = wrPools.getWaitReq(minPoolIDLen * 2 * 2 * 2)
+	assert.Equal(t, minPoolIDLen*2*2*2, cap(wr.ids))
+	wr.release()
+	wr = wrPools.getWaitReq(maxPoolIDLen)
+	assert.Equal(t, maxPoolIDLen, cap(wr.ids))
+	wr.release()
+	wr = wrPools.getWaitReq(maxPoolIDLen + 1)
+	assert.Equal(t, maxPoolIDLen+1, cap(wr.ids))
+	wr.release()
+}
